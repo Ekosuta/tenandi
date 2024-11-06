080a26b19843bb1d7f0e5a610248cdec624f0adc
Elvin Kosuta <elvinkousta6@gmail.com>
2024-11-05 23:31:12 -0800

    updated main window

--- app/main_window.py
+++ app/main_window.py
@@ -7,15 +7,17 @@ class MainWindow(ctk.CTk):
         sw = self.winfo_screenwidth()
         sh = self.winfo_screenheight()
         w = 500
-        h = 400
+        h = 450
         x = int(sw/2 + w)
-        y = int(sh/2 + h/6)
+        y = int(sh/2)
         self.geometry(f'{w}x{h}+{x}+{y}')
         self.grid_columnconfigure((0, 1, 2), weight=1)
+        self._set_appearance_mode('system')
+        
 
         # search bar
         self.search_entry = ctk.CTkEntry(self, placeholder_text='Search in English or Tenandi')
-        self.search_entry.grid(row=0, column=1, sticky='ew', pady=(20, 10) )
+        self.search_entry.grid(row=0, column=1, sticky='ew', pady=(20, 10))
 
         # search button
         self.search_button = ctk.CTkButton(self, text='search', command=self.search)
@@ -23,3 +25,5 @@ class MainWindow(ctk.CTk):
 
     def search(self):
         pass
+
+    
