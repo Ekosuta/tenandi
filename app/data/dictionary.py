908abd64970a077564ee8026e4cee71acec0a67a
Elvin Kosuta <elvinkousta6@gmail.com>
2024-11-09 18:16:19 -0800

    added tabview for search results, todo: fix recursion error

--- app/data/dictionary.py
+++ app/data/dictionary.py
@@ -11,14 +11,14 @@ def load_dictionary():
         ten_ex = row['TenandiExample']
         
         dictionary[tenandi] = {
-            'english': english,
+            'translation': english,
             'part_of_speech': part_of_speech,
             'english_example': eng_ex,
             'tenandi_example': ten_ex
         }
         
         dictionary[english] = {
-            'tenandi': tenandi,
+            'translation': tenandi,
             'part_of_speech': part_of_speech,
             'english_example': eng_ex,
             'tenandi_example': ten_ex
@@ -31,5 +31,7 @@ def load_dictionary():
 dictionary_data = load_dictionary()
 
 #TESTING
-result = dictionary_data.get('hello')
-print(result['tenandi'])
+result1 = dictionary_data.get('hello')
+result2 = dictionary_data.get('ale')
+print(result1['translation'])
+print(result2['translation'])
--- app/main_window.py
+++ app/main_window.py
@@ -15,11 +15,12 @@ class MainWindow(ctk.CTk):
         y = int(sh/2)
         self.geometry(f'{w}x{h}+{x}+{y}')
         self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
         self._set_appearance_mode('system')
         
         # search bar
         self.search_entry = ctk.CTkEntry(self, placeholder_text='Search in English or Tenandi', width=175)
-        self.search_entry.grid(row=0, column=1, pady=(20, 10))
+        self.search_entry.grid(row=1, column=1, pady=(0, 10))
 
         # search button
         self.search_button = ctk.CTkButton(
@@ -29,7 +30,7 @@ class MainWindow(ctk.CTk):
             corner_radius=10,
             width=100
             )
-        self.search_button.grid(row=1, column=1, pady=(0, 10))
+        self.search_button.grid(row=1, column=1, pady=(70, 10))
 
         # enter button configuration
         self.bind('<Return>', lambda event: self.handle_search())
@@ -41,11 +42,26 @@ class MainWindow(ctk.CTk):
         if term in dictionary_data and term in conjugation_data:
             return result1, result2
         elif term in dictionary_data:
+            self.word_result_frame()
             return result1
-        elif term in conjugation_data:
-            return result2
         
-    def result_frame(self):
-        self.result_frame = ctk.CTkFrame(self, )
+    def word_result_frame(self):
+        self.word_frame = ctk.CTkTabview(self, width=400, height=400, fg_color=('#D1E4BB', '#929E84'))
+        self.word_frame.grid(row=2, columnspan=3)
+
+        # creating tabs and result object
+        self.word_frame.add('Translation')
+        self.word_frame.add('Morphology')
+        self.word_frame.add('Prounciation')
+        result = self.handle_search()
+
+        # translation tab widgets
+        self.word = ctk.CTkLabel(self.word_frame.tab('Translation'), text=(result['translation']))
+        self.word.pack()
+
+        # morphology tab widgets
+
+
+        # pronunciation tab widgets
 
     
